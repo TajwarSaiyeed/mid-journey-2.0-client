84a63752a52b6a788c7096a3d9dfd8fd657ec800
TajwarSaiyeed <rjabid36@gmail.com>
2023-02-20 15:14:39 +0600

    HOME | Search Text

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,6 +5,7 @@ import { Loader, FormField, Card } from "../components";
 const Home = () => {
   const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState(null);
+  const [searchText, setSearchText] = useState("");
 
   return (
     <section className="max-w-7xl mx-auto">
@@ -28,7 +29,15 @@ const Home = () => {
             <Loader />
           </div>
         ) : (
-          <></>
+          <>
+            {searchText && (
+              <h2 className="font-medium text-[#666e75] text-xl mb-3">
+                Showing results for{" "}
+                <span className="text-[#222328]">{searchText}</span>
+              </h2>
+            )}
+            <div className="grid"></div>
+          </>
         )}
       </div>
     </section>
